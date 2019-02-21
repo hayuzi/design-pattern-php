1b47c05208bcf1cb77e66cc1378d44290622096d
yuzi <hayuzi@163.com>
2019-02-21 15:26:49 +0800

    init

---
+++ Lib/DesignPattern/FactoryMethod/FactoryMethodSample.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuzi
+ * Date: 2019/2/21
+ * Time: 10:58
+ */
+
+namespace Lib\DesignPattern\FactoryMethod;
+
+
+class FactoryMethodSample
+{
+    /**
+     * 简单工厂：工厂类中，这个工厂类提供一个创建具体产品类的方法，可以依照具体参数来创建不同的产品类。 ( 不在23中设计模式中 )
+     * 但是简单工厂违反了开闭原则， 不便扩展
+     *
+     * @throws \Exception
+     */
+    public static function sample()
+    {
+
+    }
+
+}
