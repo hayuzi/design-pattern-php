fdd111b46c5d8306a7694e31e47b970e3f4cda04
yuzi <hayuzi@163.com>
2019-02-21 15:24:00 +0800

    init

--- Lib/DesignPattern/SimpleFactory/SimpleFactorySample.php
+++ Lib/DesignPattern/SimpleFactory/SimpleFactorySample.php
@@ -13,6 +13,9 @@ class SimpleFactorySample
 {
 
     /**
+     * 简单工厂：工厂类中，这个工厂类提供一个创建具体产品类的方法，可以依照具体参数来创建不同的产品类。 ( 不在23中设计模式中 )
+     * 但是简单工厂违反了开闭原则， 不便扩展
+     *
      * @throws \Exception
      */
     public static function sample()
