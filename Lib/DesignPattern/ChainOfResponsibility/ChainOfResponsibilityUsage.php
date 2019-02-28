53cdacc85db31cac8b6e84c8ba3380ec43b4d76c
yuzi <hayuzi@163.com>
2019-02-28 10:13:53 +0800

    init

---
+++ Lib/DesignPattern/ChainOfResponsibility/ChainOfResponsibilityUsage.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuzi
+ * Date: 2019/2/27
+ * Time: 10:46
+ */
+
+namespace Lib\DesignPattern\ChainOfResponsibility;
+
+
+use Lib\DesignPattern\UsageInterface;
+
+class ChainOfResponsibilityUsage implements UsageInterface
+{
+
+    public static function sample()
+    {
+        /**
+         *
+         *
+         *
+         */
+
+
+    }
+
+
+}
